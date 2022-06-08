6ea1bc27cd76a6142fb46fafea90155b55025033
souayrioss <souayri.oussama@gmail.com>
2022-06-08 20:25:07 +0100

    add footer

---
+++ App/views/inc/footer.php
@@ -0,0 +1,40 @@
+<footer class="px-12 py-4">
+    <div class="grid grid-cols-3 ">
+        <div class="flex gap-5 items-center justify-center">
+            <img class="md:h-16" src="<?php echo URLROOT ;?>/img/cash.svg" alt="paiment">
+            <div>
+                <h3 class="font-semibold text-xl">Paiement à la livraison</h3>
+                <span class="font-light ">Livraison gratuite à partir de 499 MAD</span>
+            </div>
+        </div>
+        <div class="flex gap-5 items-center justify-center">
+            <img class="md:h-16" src="<?php echo URLROOT ;?>/img/magazan.svg" alt="paiment">
+            <div>
+                <span class="font-light ">Remplacement de produit en magasin</span>
+            </div>
+        </div>
+        <div class="flex gap-5 items-center justify-center ">
+            <img class="md:h-16" src="<?php echo URLROOT ;?>/img/return.svg" alt="paiment">
+            <div>
+                <h3 class="font-semibold text-xl">Retour sous 30 jours</h3>
+                <span class="font-light ">Retour facile et gratuit</span>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-rows-3 place-content-center mt-20 gap-14">
+        <h2 class="uppercase text-2xl font-bold text-center">abonnez-vous à notre newsletter </h2>
+        <ul class="flex justify-center gap-8">
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">twitter</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">Facebook</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">Etsy</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">instagram</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">Youtube</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">Printrest</li>
+            <li class="uppercase text-sm font-extralight text-slate-400 tracking-tighter">Tiktok</li>
+        </ul>
+        <span class="uppercase text-xs font-extralight text-slate-600 tracking-widest "><a href="#" >Pramètres des cookies</a> | <a href="#">POLITIQUE DE CONFIDENTIALITÉ ET UTILISATION DES COOKIES </a> | <a href="#">CONDITIONS DE VENTE </a> </span>
+    </div>
+</footer>
+<script src="<?php echo URLROOT ?>/js/main.js"></script>
+</body>
+</html>
