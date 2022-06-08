1ccc2bfcabe2e820fb6a303c028f8f2ada880a91
souayrioss <souayri.oussama@gmail.com>
2022-06-08 16:31:47 +0100

    config talwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+      "./App/views/*.{html,js,php}",
+      "./App/views/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        "sans": ["Open Sans", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
