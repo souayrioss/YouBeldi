300e36c1fe468f1a86c833585ca1ed60edd8ef9a
souayrioss <souayri.oussama@gmail.com>
2022-06-12 23:00:21 +0100

    add about page

--- App/views/client/about.php
+++ App/views/client/about.php
@@ -1,5 +1,6 @@
 <?php require APPROOT .'/views/inc/header.php';?>
 <?php require APPROOT .'/views/inc/navbar.php';?>
+
 <main class="snap-y snap-mandatory h-screen overflow-scroll">
         <section class="snap-start w-screen h-[620px] grid grid-cols-12 content-center items-center">
             <div class="col-start-3 col-end-8" >
@@ -91,7 +92,3 @@
             <?php require APPROOT .'/views/inc/footer.php';?>
         </section>
 </main>
-
-
-
-
