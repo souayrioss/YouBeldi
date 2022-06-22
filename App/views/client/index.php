e8892bebf98d5d8d1c185e401a3c40d97c403725
souayrioss <souayri.oussama@gmail.com>
2022-06-22 19:21:44 +0100

    app view fix prd

--- App/views/client/index.php
+++ App/views/client/index.php
@@ -5,32 +5,32 @@
     <div class="relative mb-10">
         <img   src="<?php echo URLROOT ;?>/img/woman_trend.webp" alt="">
         <h2 class="absolute bottom-60 left-[600px]  text-white text-4xl font-bold tracking-wider">NOUVELLES ARRIVÉES</h2>
-        <a href="client" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
+        <a href="<?php echo URLROOT ;?>/Produits/genre/femme" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
     </div>
-        <div class="w-full flex gap-5 justify-center items-center px-5 " id="trend-slider">
-            <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div>
-            <div><a href="ccccc"><img class="" src="<?php echo URLROOT ;?>/img/woman_2.webp" alt=""></a></div>
+        <div class="w-full flex gap-5 justify-center items-center px-5 " i>
+            <!-- <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div> -->
+            <div ><a href="#"><img class="" src="<?php echo URLROOT ;?>/img/woman_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_3.webp" alt=""></a></div>
-            <div><a><img class="h-80 w-[600px] " src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
+            <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_3.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
-            <div class="ml-2"><i class="fa-solid fa-arrow-right-long text-2xl"></i></div>
+            <!-- <div class="ml-2"><i class="fa-solid fa-arrow-right-long text-2xl"></i></div> -->
         </div>
 </section>
 <section class="mb-20">
     <div class="relative mb-10">
         <img  src="<?php echo URLROOT ;?>/img/man_trend.webp" alt="">
         <h2 class="absolute bottom-60 left-[600px]  text-white text-4xl font-bold tracking-wider">NOUVELLES ARRIVÉES</h2>
-        <a href="client" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
+        <a href="<?php echo URLROOT ;?>/Produits/genre/homme" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
     </div>
         <div class="w-full flex gap-5 justify-center items-center px-5" id="trend-slider">
-            <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div>
+            <!-- <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div> -->
             <div><a href="ccccc"><img class="" src="<?php echo URLROOT ;?>/img/man_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_3.webp" alt=""></a></div>
-            <div><a><img class="h-80 w-[600px] " src="<?php echo URLROOT ;?>/img/man_1.webp" alt=""></a></div>
+            <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_3.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/man_1.webp" alt=""></a></div>
@@ -41,10 +41,10 @@
     <div class="relative mb-10">
         <img  src="<?php echo URLROOT ;?>/img/kids_trend.webp" alt="">
         <h2 class="absolute bottom-60 left-[600px]  text-white text-4xl font-bold tracking-wider">NOUVELLES ARRIVÉES</h2>
-        <a href="client" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
+        <a href="<?php echo URLROOT ;?>/Produits/genre/enfant" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
     </div>
     <div class="w-full flex gap-5 justify-center items-center px-5" id="trend-slider">
-            <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div>
+            <!-- <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div> -->
             <div><a href="ccccc"><img class="" src="<?php echo URLROOT ;?>/img/kids_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/kids_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/kids_3.webp" alt=""></a></div>
