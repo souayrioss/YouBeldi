9d4a671a2d7c76d38c7a50682c22f676834b6052
souayrioss <souayri.oussama@gmail.com>
2022-06-23 06:58:12 +0100

    fixed panier

--- App/views/products/panier.php
+++ App/views/products/panier.php
@@ -70,7 +70,7 @@
                 </div>
             </div>
         </div>
-            <div class=" bg-gray-50  w-full h-[500px] xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
+            <div class=" bg-gray-50  w-full h-[600px] xl:w-[28rem] flex justify-between items-center md:items-start px-2 py-4 md:p-4 xl:p-8 flex-col">
                 <h3 class="text-xl  font-semibold leading-5 text-gray-800">CLIENT</h3>
                 <div class="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
                     <div class="flex flex-col justify-start items-start flex-shrink-0">
@@ -79,27 +79,30 @@
                                 <p class="text-base  font-semibold leading-4 text-left text-gray-800"><?php echo $data['paniers'][0]->nom . ' ' . $data['paniers'][0]->prenom  ?></p>
                             </div>
                         </div>
-
                         <div class="flex justify-center text-gray-800  md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
                             <p class="cursor-pointer text-sm leading-5 "><?php echo $data['paniers'][0]->email?></p>
                         </div>
                     </div>
-                    <div class="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
+                    <form method="POST" action="<?php echo URLROOT; ?>/Commandes/addCommande">
+                        <input type="hidden" name="prixCmd" value="" id="prixCmd">
+                    <div class="flex xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
                         <div class="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
-                            <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
+                            <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-4 w-full">
                                 <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">Adresse de livraison</p>
-                                <textarea type="text" class=" border border-black text-gray-900 text-sm  outline-none block w-full p-2  " placeholder="" required> </textarea>
-                                
-                            </div>
-                            <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
-                                <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">Billing Address</p>
-                                <p class="w-48 lg:w-full  xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+                                <textarea type="text" name="adresse" class=" border border-black text-gray-900 text-sm  outline-none block w-full p-2  " placeholder="" required> </textarea>
+                                <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">Phone</p>
+                                <input type="text" name="tel" class=" border border-black text-gray-900 text-sm  outline-none block w-full p-2  " required>
+                                <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">CIN</p>
+                                <input type="text" name="cin" class=" border border-black text-gray-900 text-sm  outline-none block w-full p-2  " required>
                             </div>
                         </div>
-                        <div class="flex w-full justify-center items-center md:justify-start md:items-start">
-                            <button class="mt-6 md:mt-0 dark:border-white dark:hover:bg-gray-900 dark:bg-transparent  py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800  w-96 2xl:w-full text-base font-medium leading-4 text-gray-800">Edit Details</button>
+                        <div class="flex w-full justify-center items-center md:justify-start md:items-start mt-8">
+
+                            <button type="submit" class="uppercase w-4/5 text-base font-semibold text-white inline-block text-center bg-black border border-white py-3 hover:bg-gray-900 ">Commander</button>
+                            <button type="submit" class="uppercase w-1/5 text-xl font-semibold inline-block text-center p-2.5 bg-white border border-black  hover:bg-gray-900 hover:text-white"><i class="fa-brands fa-paypal"></i></button>
                         </div>
                     </div>
+                    </form>
                 </div>
             </div>
         </div>
