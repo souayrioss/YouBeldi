a0da92b416e2c64b29a03a326794cb880b17c45b
souayrioss <souayri.oussama@gmail.com>
2022-06-23 07:43:48 +0100

    index view client

--- App/views/client/index.php
+++ App/views/client/index.php
@@ -8,7 +8,7 @@
         <a href="<?php echo URLROOT ;?>/Produits/genre/femme" class="absolute bottom-40 left-[740px] text-base font-bold text-white inline-block text-center border-2 border-white  px-10 py-3   hover:font-bold transition ">Femme</a>
     </div>
         <div class="w-full flex gap-5 justify-center items-center px-5 " i>
-            <!-- <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div> -->
+            <div class="mr-2"><i class="fa-solid fa-arrow-left-long text-2xl "></i></div>
             <div ><a href="#"><img class="" src="<?php echo URLROOT ;?>/img/woman_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_3.webp" alt=""></a></div>
@@ -16,7 +16,7 @@
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_2.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_3.webp" alt=""></a></div>
             <div><a><img class="" src="<?php echo URLROOT ;?>/img/woman_1.webp" alt=""></a></div>
-            <!-- <div class="ml-2"><i class="fa-solid fa-arrow-right-long text-2xl"></i></div> -->
+            <div class="ml-2"><i class="fa-solid fa-arrow-right-long text-2xl"></i></div>
         </div>
 </section>
 <section class="mb-20">
